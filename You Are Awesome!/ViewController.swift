db47736257a25b1f439d3e25f17c651e9411f121
Michael Green <MGreen@michaels-air.bc.edu>
2018-01-22 17:37:56 -0500

    uploading file

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -6,20 +6,59 @@
 //  Copyright © 2018 mgreen. All rights reserved.
 //
 
-import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    import UIKit
+    
+    class ViewController: UIViewController {
+        
+        @IBOutlet weak var messageLabel: UILabel!
+        var index1 = 0
+        var index2 = 0
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+        
+        
+        @IBAction func showMessagePressed(_ sender: Any) {
+            var messages = ["You Are Awesome!", "You Are Great!", "You Are Da Bomb!", "When the Genius Bar Needs Help They Call You!"]
+            var colors = [UIColor.red, UIColor.blue, UIColor.green]
+            
+            
+            messageLabel.text = messages[index1]
+            messageLabel.textColor = colors[index2]
+            
+            index1 = index1 + 1
+            
+            if index1 == messages.count{
+                index1 = 0
+            }
+            
+            index2 = index2 + 1
+            
+            if index2 == colors.count{
+                index2 = 0
+            }
+            
+            
+            //        let message1 = "You Are Awesome!"
+            //        let message2 = "You Are Great!"
+            //
+            //        if messageLabel.text == message1 {
+            //            messageLabel.text = "You are Great"
+            //            messageLabel.textColor = UIColor.blue
+            //        } else {
+            //            messageLabel.text = message2
+            //            messageLabel.textColor = UIColor.red
+            //        }
+        }
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-
-}
 
